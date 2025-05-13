86ec34ff25a1841dae833805782e9820e68abe54
Cristian Sarbu <chris.56es@gmail.com>
2025-05-13 14:59:30 +0000

    Partially implements pagination functionality for /credentials

--- controllers/credentialscontroller.js
+++ controllers/credentialscontroller.js
@@ -1,22 +1,31 @@
 const axios = require("axios");
-const datosJson = require("../public/misc/test.json")
+const datosJson = require("../public/misc/test.json");
 
 module.exports = {
   get: (req, res) => {
-    // axios
-    //   .get(
-    //     "https://ces-main.lab.gaia-x.eu/credentials-events?page=0&size=20&type=eu.gaia-x.credential"
-    //   )
-    //   .then((response) => {
-    //     // const datos = JSON.stringify(response.data);
-    //     const datosJson = response.data;
-    //     res.render("index", { datosJson });
-    //   })
-    //   .catch((error) => {
-    //     res.send("Error fetching data: " + error);
-    //   });
+    const datosParams = req.query;
+    datosParams.page = parseInt(datosParams.page);
+
+    const config = {
+        params: {
+            page: '1',
+            size: '5',
+            type: 'eu.gaia-x.credential'
+        }
+    }
+
+    axios
+      .get("https://ces-main.lab.gaia-x.eu/credentials-events", config)
+      .then((response) => {
+        // const datos = JSON.stringify(response.data);
+        const datosJson = response.data;
+        res.render("index", { datosJson, datosParams });
+      })
+      .catch((error) => {
+        res.send("Error fetching data: " + error);
+      });
 
     // Pruebas con json local "/public/misc/test.json" para ahorrar solicitudes a API real
-    res.render("credentials", { datosJson })
+    // res.render("credentials", { datosJson, datosParams })
   },
 };
