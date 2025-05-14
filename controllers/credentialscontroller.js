4406266b4a1d08db597430915b268e78df8d6bd3
Cristian Sarbu <chris.56es@gmail.com>
2025-05-14 08:42:06 +0000

    Finishes pagination functionality with 20 elements per page and dynamic query parameters for the GaiaX API request. Defaults no query param to page 0.

--- controllers/credentialscontroller.js
+++ controllers/credentialscontroller.js
@@ -4,28 +4,26 @@ const datosJson = require("../public/misc/test.json");
 module.exports = {
   get: (req, res) => {
     const datosParams = req.query;
-    datosParams.page = parseInt(datosParams.page);
-
-    const config = {
-        params: {
-            page: '1',
-            size: '5',
-            type: 'eu.gaia-x.credential'
-        }
+    if (datosParams.page) {
+      datosParams.page = parseInt(datosParams.page);
+    } else {
+      datosParams.page = 0
     }
 
     axios
-      .get("https://ces-main.lab.gaia-x.eu/credentials-events", config)
+      .get("https://ces-main.lab.gaia-x.eu/credentials-events", {
+        params: {
+            page: datosParams.page,
+            size: 20,
+            type: 'eu.gaia-x.credential'
+        }
+      })
       .then((response) => {
-        // const datos = JSON.stringify(response.data);
         const datosJson = response.data;
-        res.render("index", { datosJson, datosParams });
+        res.render("credentials", { datosJson, datosParams });
       })
       .catch((error) => {
         res.send("Error fetching data: " + error);
       });
-
-    // Pruebas con json local "/public/misc/test.json" para ahorrar solicitudes a API real
-    // res.render("credentials", { datosJson, datosParams })
   },
 };
